703506088df3f9ac9eb1bbf8665711502f21f396
sneakwish <wishal_wishnu@hotmail.com>
2022-06-20 16:28:39 +0200

    equevelentieklassen tests

--- src/test/java/Model/BoodschappenlijstTest.java
+++ src/test/java/Model/BoodschappenlijstTest.java
@@ -97,15 +97,34 @@ class BoodschappenlijstTest {
         Assertions.assertEquals(5, lijst1.korting());
     }
 
+    @Test
     void Equivalentieklassen() {
         lijst1.setBonuskaart(false);
-        lijst1.addProduct("Banaan", 1.00);
-        lijst1.addProduct("Manderijn", 2.00);
-        lijst1.addProduct("Druif", 3.00);
-        lijst1.addProduct("Appel", 4.00);
-        lijst1.addProduct("aardbei", 4.00);
-        Assertions.assertEquals(5, lijst1.korting());
+//        Equivalentieklasse 1: 0% korting
         Assertions.assertEquals(0, lijst1.korting());
-        Assertions.assertEquals(15, lijst1.korting());
+
+        lijst1.addProduct("1", 1.00);
+        lijst1.addProduct("2", 2.00);
+        Assertions.assertEquals(0, lijst1.korting());
+
+//        Equivalentieklasse 2: 5% korting
+        lijst1.addProduct("3", 3.00);
+        lijst1.addProduct("4", 4.00);
+        lijst1.addProduct("5", 4.00);
+        lijst1.addProduct("6", 4.00);
+        Assertions.assertEquals(5, lijst1.korting());
+
+        lijst1.addProduct("7", 4.00);
+        lijst1.addProduct("8", 4.00);
+        Assertions.assertEquals(5, lijst1.korting());
+
+//        Equivalentieklasse 3: 10% korting
+        lijst1.addProduct("9", 4.00);
+        lijst1.addProduct("10", 4.00);
+        Assertions.assertEquals(10, lijst1.korting());
+
+        lijst1.addProduct("11", 4.00);
+        lijst1.addProduct("12", 4.00);
+        Assertions.assertEquals(10, lijst1.korting());
     }
 }
